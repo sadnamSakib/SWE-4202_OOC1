6a4f21eaecf62efedfc2827091f69931cd4e4104
Sadnam Sakib Apurbo <95168525+sadnamSakib@users.noreply.github.com>
2021-12-17 11:02:14 +0600

    Add files via upload

---
+++ PHARMACY_DEPENDENCY/ACCOUNT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHARMACY_DEPENDENCY
+{
+    public class ACCOUNT
+    {
+        public double accountBalance=1000;
+        public void addBalance(double n)
+        {
+            accountBalance += n;
+        }
+        public void subBalance(double n)
+        {
+            accountBalance -= n;
+        }
+        public double getBalance()
+        {
+            return accountBalance;
+
+        }
+    }
+    }
---
+++ PHARMACY_DEPENDENCY/MEDICINE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHARMACY_DEPENDENCY
+{
+    public class MEDICINE
+    {
+        public string medicineName;
+        public double sellPrice, buyPrice;
+        public int medicineAmount;
+
+        public string getMedicineInfo()
+        {
+            string medName = Convert.ToString(medicineAmount);
+            return medicineName + "\t\t\t"  + medName;
+        }
+    }
+}
