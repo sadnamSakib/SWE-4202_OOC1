b56806d74969541e09efa49049525f8150104642
Sadnam Sakib Apurbo <95168525+sadnamSakib@users.noreply.github.com>
2022-03-22 21:03:32 +0600

    Add files via upload

---
+++ LAB FINAL IUTRPS/IUTRPS/Form1.Designer.cs
@@ -0,0 +1,403 @@
+﻿namespace IUTRPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.idListBox = new System.Windows.Forms.ListBox();
+            this.gradeListBox = new System.Windows.Forms.ListBox();
+            this.percentageListBox = new System.Windows.Forms.ListBox();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.idTextbox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.attendanceLabel = new System.Windows.Forms.Label();
+            this.midLabel = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.quizTotalLabel = new System.Windows.Forms.Label();
+            this.quiz4Label = new System.Windows.Forms.Label();
+            this.quiz3Label = new System.Windows.Forms.Label();
+            this.quiz2Label = new System.Windows.Forms.Label();
+            this.quiz1Label = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.finalLabel = new System.Windows.Forms.Label();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.percentageLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.vivaLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ROG Fonts", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(410, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(644, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IUT RESULT PROCESSING SYSTEM";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(58, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(677, 10);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(201, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 452);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(453, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 452);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(606, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 452);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(120, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "ID";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(657, 58);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "GRADE";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(500, 58);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "PERCENTAGE";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(305, 58);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 16);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "NAME";
+            // 
+            // idListBox
+            // 
+            this.idListBox.FormattingEnabled = true;
+            this.idListBox.ItemHeight = 16;
+            this.idListBox.Location = new System.Drawing.Point(61, 107);
+            this.idListBox.Name = "idListBox";
+            this.idListBox.Size = new System.Drawing.Size(134, 452);
+            this.idListBox.TabIndex = 10;
+            // 
+            // gradeListBox
+            // 
+            this.gradeListBox.FormattingEnabled = true;
+            this.gradeListBox.ItemHeight = 16;
+            this.gradeListBox.Location = new System.Drawing.Point(636, 103);
+            this.gradeListBox.Name = "gradeListBox";
+            this.gradeListBox.Size = new System.Drawing.Size(99, 452);
+            this.gradeListBox.TabIndex = 11;
+            // 
+            // percentageListBox
+            // 
+            this.percentageListBox.FormattingEnabled = true;
+            this.percentageListBox.ItemHeight = 16;
+            this.percentageListBox.Location = new System.Drawing.Point(494, 107);
+            this.percentageListBox.Name = "percentageListBox";
+            this.percentageListBox.Size = new System.Drawing.Size(106, 452);
+            this.percentageListBox.TabIndex = 12;
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.ItemHeight = 16;
+            this.nameListBox.Location = new System.Drawing.Point(217, 107);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(218, 452);
+            this.nameListBox.TabIndex = 13;
+            // 
+            // idTextbox
+            // 
+            this.idTextbox.Location = new System.Drawing.Point(968, 93);
+            this.idTextbox.Name = "idTextbox";
+            this.idTextbox.Size = new System.Drawing.Size(206, 22);
+            this.idTextbox.TabIndex = 14;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(893, 93);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 16);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "ID : ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(968, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Search By ID";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // attendanceLabel
+            // 
+            this.attendanceLabel.AutoSize = true;
+            this.attendanceLabel.Location = new System.Drawing.Point(968, 199);
+            this.attendanceLabel.Name = "attendanceLabel";
+            this.attendanceLabel.Size = new System.Drawing.Size(106, 16);
+            this.attendanceLabel.TabIndex = 17;
+            this.attendanceLabel.Text = "ATTENDANCE :";
+            // 
+            // midLabel
+            // 
+            this.midLabel.AutoSize = true;
+            this.midLabel.Location = new System.Drawing.Point(968, 408);
+            this.midLabel.Name = "midLabel";
+            this.midLabel.Size = new System.Drawing.Size(40, 16);
+            this.midLabel.TabIndex = 18;
+            this.midLabel.Text = "MID : ";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(968, 408);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 16);
+            this.label13.TabIndex = 19;
+            // 
+            // quizTotalLabel
+            // 
+            this.quizTotalLabel.AutoSize = true;
+            this.quizTotalLabel.Location = new System.Drawing.Point(968, 371);
+            this.quizTotalLabel.Name = "quizTotalLabel";
+            this.quizTotalLabel.Size = new System.Drawing.Size(151, 16);
+            this.quizTotalLabel.TabIndex = 20;
+            this.quizTotalLabel.Text = "QUIZ TOTAL (BEST 3 ) :";
+            // 
+            // quiz4Label
+            // 
+            this.quiz4Label.AutoSize = true;
+            this.quiz4Label.Location = new System.Drawing.Point(968, 334);
+            this.quiz4Label.Name = "quiz4Label";
+            this.quiz4Label.Size = new System.Drawing.Size(57, 16);
+            this.quiz4Label.TabIndex = 21;
+            this.quiz4Label.Text = "QUIZ 4 : ";
+            // 
+            // quiz3Label
+            // 
+            this.quiz3Label.AutoSize = true;
+            this.quiz3Label.Location = new System.Drawing.Point(968, 297);
+            this.quiz3Label.Name = "quiz3Label";
+            this.quiz3Label.Size = new System.Drawing.Size(57, 16);
+            this.quiz3Label.TabIndex = 22;
+            this.quiz3Label.Text = "QUIZ 3 : ";
+            // 
+            // quiz2Label
+            // 
+            this.quiz2Label.AutoSize = true;
+            this.quiz2Label.Location = new System.Drawing.Point(968, 265);
+            this.quiz2Label.Name = "quiz2Label";
+            this.quiz2Label.Size = new System.Drawing.Size(57, 16);
+            this.quiz2Label.TabIndex = 23;
+            this.quiz2Label.Text = "QUIZ 2 : ";
+            // 
+            // quiz1Label
+            // 
+            this.quiz1Label.AutoSize = true;
+            this.quiz1Label.Location = new System.Drawing.Point(968, 230);
+            this.quiz1Label.Name = "quiz1Label";
+            this.quiz1Label.Size = new System.Drawing.Size(57, 16);
+            this.quiz1Label.TabIndex = 24;
+            this.quiz1Label.Text = "QUIZ 1 : ";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(981, 587);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(0, 16);
+            this.label19.TabIndex = 25;
+            // 
+            // finalLabel
+            // 
+            this.finalLabel.AutoSize = true;
+            this.finalLabel.Location = new System.Drawing.Point(968, 439);
+            this.finalLabel.Name = "finalLabel";
+            this.finalLabel.Size = new System.Drawing.Size(53, 16);
+            this.finalLabel.TabIndex = 26;
+            this.finalLabel.Text = "FINAL : ";
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.AutoSize = true;
+            this.gradeLabel.Location = new System.Drawing.Point(968, 556);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(64, 16);
+            this.gradeLabel.TabIndex = 27;
+            this.gradeLabel.Text = "GRADE : ";
+            // 
+            // percentageLabel
+            // 
+            this.percentageLabel.AutoSize = true;
+            this.percentageLabel.Location = new System.Drawing.Point(968, 526);
+            this.percentageLabel.Name = "percentageLabel";
+            this.percentageLabel.Size = new System.Drawing.Size(109, 16);
+            this.percentageLabel.TabIndex = 28;
+            this.percentageLabel.Text = "PERCENTAGE : ";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(968, 499);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(145, 16);
+            this.totalLabel.TabIndex = 29;
+            this.totalLabel.Text = "TOTAL (OUT OF 300) : ";
+            // 
+            // vivaLabel
+            // 
+            this.vivaLabel.AutoSize = true;
+            this.vivaLabel.Location = new System.Drawing.Point(968, 469);
+            this.vivaLabel.Name = "vivaLabel";
+            this.vivaLabel.Size = new System.Drawing.Size(46, 16);
+            this.vivaLabel.TabIndex = 30;
+            this.vivaLabel.Text = "VIVA : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1487, 624);
+            this.Controls.Add(this.vivaLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.percentageLabel);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.finalLabel);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.quiz1Label);
+            this.Controls.Add(this.quiz2Label);
+            this.Controls.Add(this.quiz3Label);
+            this.Controls.Add(this.quiz4Label);
+            this.Controls.Add(this.quizTotalLabel);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.midLabel);
+            this.Controls.Add(this.attendanceLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.idTextbox);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.percentageListBox);
+            this.Controls.Add(this.gradeListBox);
+            this.Controls.Add(this.idListBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox idListBox;
+        private System.Windows.Forms.ListBox gradeListBox;
+        private System.Windows.Forms.ListBox percentageListBox;
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.TextBox idTextbox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label attendanceLabel;
+        private System.Windows.Forms.Label midLabel;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label quizTotalLabel;
+        private System.Windows.Forms.Label quiz4Label;
+        private System.Windows.Forms.Label quiz3Label;
+        private System.Windows.Forms.Label quiz2Label;
+        private System.Windows.Forms.Label quiz1Label;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label finalLabel;
+        private System.Windows.Forms.Label gradeLabel;
+        private System.Windows.Forms.Label percentageLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label vivaLabel;
+    }
+}
+
---
+++ LAB FINAL IUTRPS/IUTRPS/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IUTRPSDEPENDENCIES;
+using System.IO;
+
+namespace IUTRPS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            using (var reader = new StreamReader(@"D:\OOC LAB\SWE4201MarkSheet.csv"))
+            {
+
+                
+                while (!reader.EndOfStream)
+                {
+                    
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    double attendance;
+                    double quiz1;
+                    double quiz2;
+                    double quiz3;
+                    double quiz4;
+                    double mid;
+                    double final;
+                    double viva;
+                    if (values[2] == "") {
+                        attendance = 0;
+                    
+                    }
+                    else
+                    {
+                        attendance = Convert.ToDouble(values[2]);
+                    }
+                    if (values[3] == "")
+                    {
+                        quiz1 = 0;
+
+                    }
+                    else
+                    {
+                        quiz1 = Convert.ToDouble(values[3]);
+                    }
+                    if (values[4] == "")
+                    {
+                        quiz2 = 0;
+
+                    }
+                    else
+                    {
+                        quiz2 = Convert.ToDouble(values[4]);
+                    }
+                    if (values[5] == "")
+                    {
+                        quiz3 = 0;
+
+                    }
+                    else
+                    {
+                        quiz3 = Convert.ToDouble(values[5]);
+                    }
+                    if (values[6] == "")
+                    {
+                        quiz4 = 0;
+
+                    }
+                    else
+                    {
+                        quiz4 = Convert.ToDouble(values[6]);
+                    }
+                    if (values[7] == "")
+                    {
+                        mid = 0;
+
+                    }
+                    else
+                    {
+                        mid = Convert.ToDouble(values[7]);
+                    }
+                    if (values[8] == "")
+                    {
+                        final = 0;
+
+                    }
+                    else
+                    {
+                        final = Convert.ToDouble(values[8]);
+                    }
+                    if (values[9] == "")
+                    {
+                        viva = 0;
+
+                    }
+                    else
+                    {
+                        viva = Convert.ToDouble(values[9]);
+                    }
+
+                    STUDENT dummy_student = new STUDENT(values[0], values[1],attendance, quiz1, quiz2, quiz3, quiz4, mid, final, viva);
+
+                    List<double> quizes = new List<double>();
+
+                    quizes.Add(dummy_student.quiz1);
+                    quizes.Add(dummy_student.quiz2);
+                    quizes.Add(dummy_student.quiz3);
+                    quizes.Add(dummy_student.quiz4);
+                    quizes.Sort();
+
+                    dummy_student.total = attendance + quizes[3] +quizes[2]+quizes[1]+ mid + final + viva;
+                    dummy_student.percentage = (dummy_student.total / 3);
+                    if (dummy_student.percentage >= 80)
+                    {
+                        dummy_student.grade = "A+";
+                    }
+                    else if(dummy_student.percentage <80 && dummy_student.percentage > 75)
+                    {
+                        dummy_student.grade = "A";
+                    }
+                    else if (dummy_student.percentage < 75 && dummy_student.percentage > 70)
+                    {
+                        dummy_student.grade = "A-";
+                    }
+                    else if (dummy_student.percentage < 70 && dummy_student.percentage > 65)
+                    {
+                        dummy_student.grade = "B+";
+                    }
+                    else if (dummy_student.percentage < 65 && dummy_student.percentage > 60)
+                    {
+                        dummy_student.grade = "B";
+                    }
+                    else if (dummy_student.percentage < 60 && dummy_student.percentage > 55)
+                    {
+                        dummy_student.grade = "C";
+                    }
+                    else if (dummy_student.percentage < 55 && dummy_student.percentage > 50)
+                    {
+                        dummy_student.grade = "D";
+                    }
+                    else
+                    {
+                        dummy_student.grade = "F";
+                    }
+
+
+                    RPS.students.Add(dummy_student);
+                    idListBox.Items.Add(dummy_student.studentID);
+                    nameListBox.Items.Add(dummy_student.studentName);
+                    percentageListBox.Items.Add(Convert.ToString(Math.Round(dummy_student.percentage,2)));
+                    gradeListBox.Items.Add(dummy_student.grade);
+
+
+                   
+
+                }
+            }
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = idTextbox.Text;
+            for(int i = 0; i < RPS.students.Count;i++)
+            {
+                if (id == RPS.students[i].studentID)
+                {
+                    STUDENT dummy = RPS.students[i];
+                    attendanceLabel.Text = "Attendance : " + RPS.students[i].studentAttendance;
+                    quiz1Label.Text = "Quiz 1 : " + RPS.students[i].quiz1;
+                    quiz2Label.Text = "Quiz 2 : " + RPS.students[i].quiz2;
+                    quiz3Label.Text = "Quiz 3 : " + RPS.students[i].quiz3;
+                    quiz4Label.Text = "Quiz 4 : " + RPS.students[i].quiz4;
+
+                    List<double> quizes2 = new List<double>();
+
+                    quizes2.Add(dummy.quiz1);
+                    quizes2.Add(dummy.quiz2);
+                    quizes2.Add(dummy.quiz3);
+                    quizes2.Add(dummy.quiz4);
+                    quizes2.Sort();
+                    double temp = 0;
+                    for(int j = 1; j < 4; j++)
+                    {
+                        temp += quizes2[j];
+                    }
+                    quizTotalLabel.Text = "Quiz Total : " + temp;
+                    midLabel.Text = "Mid : " + dummy.mid;
+                    finalLabel.Text = "Final : " + dummy.final;
+                    vivaLabel.Text = "Viva : " + dummy.viva;
+                    totalLabel.Text = "Total : " + Math.Round(dummy.total,2);
+                    percentageLabel.Text = "Percentage : " + Math.Round(dummy.percentage,2);
+                    gradeLabel.Text = "Grade : " + dummy.grade;
+                    break;
+                }
+            }
+        }
+    }
+}
